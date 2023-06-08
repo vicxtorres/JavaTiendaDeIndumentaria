3b496f7150e5326591c4b6f0483e7ce75a008a80
vicxto <vicxtorres@gmail.com>
2023-06-08 11:39:50 -0300

    Add files via upload

---
+++ tpFina/src/SistemaDeControl/Main.java
@@ -0,0 +1,8 @@
+package SistemaDeControl;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Sistema de control de Inventario");
+    }
+}
---
+++ tpFina/src/SistemaDeControl/Paquete/Paquete.java
@@ -0,0 +1,4 @@
+package SistemaDeControl.Paquete;
+
+public class Paquete {
+}
